b8d6f7026c8d44192b5326b082a5942872ecde6d
Dhalintin <uchexdhalitin@gmail.com>
2023-02-04 12:26:25 +0100

    First commit, created a landing page and styled with css

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Musixluvas</title>
+    <link rel="icon" type="image" href="images/logo1.png">
+    <link rel="stylesheet" type="text/css" href="{{ url('/style.css') }}" />
+</head>
+<body>
+    <header>
+        <a href="index.html" id="head1"><img src="images/logo-home.png">usixLuvas</a>
+        <div class="spacing">
+            <a href="#" class="head2">Home</a>
+            <a href="#" class="head2">Register</a>
+            <a href="#" class="head2">Login</a>
+        </div>
+    </header>
+
+    
+    <section>
+        <nav-left>
+            <a href="/createproduct" id="head1"><img src="images/logo-home.png">reate</a>
+            <p>Create a new product to add to the catalog</p>
+        </nav-left>
+
+        <nav-right>
+            <a href="#l" id="head1"><img src="images/logo-home.png">ead</a>
+            <p>Check out all the products in stock</p>
+        </nav-right>
+    </section>
+
+    <section>
+        <nav-left>
+            <a href="#" id="head1"><img src="images/logo-home.png">pdate</a>
+            <p>Update prices and details of products </p>
+        </nav-left>
+
+        <nav-right>
+            <a href="#" id="head1"><img src="images/logo-home.png">elete</a>
+            <p>Delete product from database</p>
+        </nav-right>
+    </section>
+
+
+   
+    <br>
+    <br>
+    <br>
+    
+    <footer>
+        <p>Designed by Dhalitin</p>
+    </footer>
+    
+
+</body>
+</html>
